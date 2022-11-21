7411ad22d3324474a953c342a118a831cef08acb
Diti Arora <diti.aromatic@gmail.com>
2022-11-22 01:39:04 +0530

    spotify api added, ts erros coming

--- pages/Post.tsx
+++ pages/Post.tsx
@@ -1,10 +1,23 @@
-import type { NextPage } from 'next';
+type Post = {
+  posts: any;
+  img: string;
+  songLink: string;
+  }
 
-const Home: NextPage = () => {
+const Home: React.FC<Post> = (posts: Post) => {
+  
   return (
-    <div>
-        heiiii
-    </div>
+    {
+      posts.map((post: Post) => 
+        <div>
+          <img className="h-40 w-52" src={post.posts.img} />
+          <h1 className='text-white'>{post.posts.desc}</h1>
+          <p>{post.posts.songLink}</p>
+        </div>
+      
+      )
+    }
+
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,80 @@
+import { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 import Post from './Post'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { BsSpotify } from 'react-icons/bs';
 
 const Home: NextPage = () => {
+  let imgLink = 'https://images.unsplash.com/photo-1546514355-7fdc90ccbd03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+
+  const [desc, setDesc] = useState('')
+  const [ search, setSearch ] = useState('')
+  const [id, setId] = useState('')
+  const [token, setToken ] = useState('') 
+  const [posts, setPosts] = useState([
+    {
+      desc: 'hehehehee',
+      img: imgLink,
+      songLink: 'ok',
+    },
+  ])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+    const id = e.target.value
+    const iditem = id.split("/")[4]
+    const idmain = iditem.split("?")[0]
+    setId(idmain)
+    console.log(idmain);
+  }
+
+  const CLIENT_ID = '987150daf0054c7ca891b415d72b457f'
+  const CLIENT_SECRET = '2e3a01bd93fe4e9b9fedc367a5185824'
+
+  useEffect(() => {
+    // ACCESS TOKEN
+    var authParams  = {
+      method: 'POST',
+      headers:  {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    }
+
+    fetch(`https://accounts.spotify.com/api/token`, authParams)
+      .then(res => res.json())
+      .then(data => setToken(data.access_token))
+
+  }, [])
+
+  const getReq = async () => {
+
+    var trackParams = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token 
+      }
+    }
+
+  await fetch(`https://api.spotify.com/v1/tracks/${id}?market=ES` , trackParams)
+    .then(resp => resp.json())
+    .then(res => {
+      // console.log(res);
+      
+      const newPost = {
+        desc: desc,
+        img: res.album.images[1].url,
+        songLink: res.external_urls.spotify,
+      } 
+
+      setPosts([...posts, newPost])
+      console.log(posts);
+      
+    }) 
+
+}
+
   return (
     <div className='bg-black imag min-h-screen w-screen overflow-x-hidden flex justify-center items-start'>
       <div className='mt-10 w-2/5 flex flex-col z-10'>
@@ -20,9 +91,9 @@ const Home: NextPage = () => {
             <div className="flex justify-between items-stretch mt-4 relative">
               <BsSpotify className='absolute green text-3xl top-2 left-4' />
 
-              <input value='https://open.spotify.com/track/6GD1eomgaGT1Epto' className='bg-white bg-opacity-10 px-3 py-2 outline-none rounded-md w-full mr-4 pl-16' type="text" />
+              <input value={search} onChange={handleChange} placeholder='https://open.spotify.com/track/6GD1eomgaGT1Epto' className='bg-white bg-opacity-10 px-3 py-2 outline-none rounded-md w-full mr-4 pl-16' type="text" />
 
-              <button className="bg-white bg-opacity-10 px-5 py-2 flex items-center text-orange custom text-2xl rounded-md">Post</button>
+              <button onClick={getReq} className="bg-white bg-opacity-10 px-5 py-2 flex items-center text-orange custom text-2xl rounded-md">Post</button>
             </div>
           </div>
         </div>
@@ -33,6 +104,9 @@ const Home: NextPage = () => {
           <p className='whitespace-nowrap text-white mx-4 custom text-2xl opacity-50'>Live Feed</p>
           <div className='border-t w-full opacity-40'></div>
         </div>
+
+        {/* Posts */}
+        <Post posts ={posts} /> 
       </div>
     </div>
   );
